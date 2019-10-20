c77e6d9ecc677eb54e697e97b9edc1ef71e41158
Chanwit Settavongsin <max180643@gmail.com>
2019-10-20 15:34:01 +0700

    :package: Add version 1.0

---
+++ SiamBIT-Thank-Auto/content.js
@@ -0,0 +1,5 @@
+var list = document.querySelectorAll('[title="กดขอบคุณที่นี่"]')
+if (list !== 'undefined' && list.length > 0) {
+    list[0].click()
+}
+// Create by max180643 (Chanwit Settavongsin)!
