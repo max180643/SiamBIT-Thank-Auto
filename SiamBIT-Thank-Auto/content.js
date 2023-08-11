56d3e4d99f68bbb7575d360b06a4556c4b2d679a
max180643 <max180643@gmail.com>
2023-08-12 02:36:19 +0700

    fix: :bug: not click thank button

--- SiamBIT-Thank-Auto/content.js
+++ SiamBIT-Thank-Auto/content.js
@@ -1,5 +1,13 @@
-var list = document.querySelectorAll('[title="กดขอบคุณที่นี่"]')
-if (list !== 'undefined' && list.length > 0) {
-    list[0].click()
+// Wait page load then find element and click
+window.onload = () => {
+  // Bypass Cloudflare Rocket Loader
+  window.__cfRLUnblockHandlers = true
+  // Find element
+  var list = document.querySelectorAll('[title="กดขอบคุณที่นี่"]')
+  // Check element is exists
+  if (list !== 'undefined' && list.length > 0) {
+      var thankButton = list[0]
+      thankButton.click()
+  }
 }
 // Create by max180643 (Chanwit Settavongsin)!
